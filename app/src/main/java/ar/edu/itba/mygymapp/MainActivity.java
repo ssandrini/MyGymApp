118f19097c1ab3e406f9b6a0915e4cf24361a4da
Santiago Sandrini <ssandrini@itba.edu.ar>
2021-10-22 09:21:36 -0300

    Agregue el binding al proyecto

--- app/src/main/java/ar/edu/itba/mygymapp/MainActivity.java
+++ app/src/main/java/ar/edu/itba/mygymapp/MainActivity.java
@@ -4,11 +4,23 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import ar.edu.itba.mygymapp.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        /*
+            Luego del binding voy a poder acceder a todos los elementos
+            del html, por ejemplo a los textView, botones, etc.
+            Ejemplo:
+            activityMainBinding.myTextView.setText(R.string.app_name);
+         */
+        
+        ActivityMainBinding activityMainBinding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(activityMainBinding.getRoot());
+
     }
 }

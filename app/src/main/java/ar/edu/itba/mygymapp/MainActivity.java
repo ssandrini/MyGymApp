c8d0b09d51092143a393babf19a8e0cca46acdd7
Leonardo <ldagostino@itba.edu.ar>
2021-10-24 17:52:21 -0300

    home activity

--- app/src/main/java/ar/edu/itba/mygymapp/MainActivity.java
+++ app/src/main/java/ar/edu/itba/mygymapp/MainActivity.java
@@ -2,7 +2,9 @@ package ar.edu.itba.mygymapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import ar.edu.itba.mygymapp.databinding.ActivityMainBinding;
 
@@ -18,9 +20,20 @@ public class MainActivity extends AppCompatActivity {
             Ejemplo:
             activityMainBinding.myTextView.setText(R.string.app_name);
          */
-        
+
+
         ActivityMainBinding activityMainBinding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(activityMainBinding.getRoot());
 
+    }
+
+    public void login(View view) {
+        Intent intent = new Intent(this, home.class);
+        startActivity(intent);
+    }
+
+    public void register(View view) {
+        Intent intent = new Intent(this, register.class);
+        startActivity(intent);
     }
 }

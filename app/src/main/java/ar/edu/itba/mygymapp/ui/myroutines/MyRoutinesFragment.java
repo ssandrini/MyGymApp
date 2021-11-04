86aa4d6936cb8ec4c8cb505be5eecbaf6477ac21
Leonardo <ldagostino@itba.edu.ar>
2021-11-04 18:22:14 -0300

    pre presentacion

--- app/src/main/java/ar/edu/itba/mygymapp/ui/myroutines/MyRoutinesFragment.java
+++ app/src/main/java/ar/edu/itba/mygymapp/ui/myroutines/MyRoutinesFragment.java
@@ -11,13 +11,16 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import ar.edu.itba.mygymapp.databinding.FragmentMyRoutinesBinding;
+import ar.edu.itba.mygymapp.ui.routines.RoutinesAdapter;
 
 public class MyRoutinesFragment extends Fragment {
 
     private MyRoutinesViewModel myRoutinesViewModel;
     private FragmentMyRoutinesBinding binding;
+    private RoutinesAdapter routinesAdapter;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -27,6 +30,11 @@ public class MyRoutinesFragment extends Fragment {
         binding = FragmentMyRoutinesBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
+        routinesAdapter = new RoutinesAdapter();
+
+        binding.destacadasRecView.setLayoutManager(new LinearLayoutManager(getContext()));
+        binding.destacadasRecView.setAdapter(routinesAdapter);
+
 //        final TextView textView = binding.textGallery;
 //        myRoutinesViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
 //            @Override
@@ -34,6 +42,8 @@ public class MyRoutinesFragment extends Fragment {
 //                textView.setText(s);
 //            }
 //        });
+
+
         return root;
     }
 

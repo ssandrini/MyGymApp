e846f2d2f54a8aecf8e0cd845155e485d45b9073
franco rodriguez tulasne <robrodriguez@itba.edu.ar>
2021-11-05 16:46:30 -0300

    registration good

--- app/src/main/java/ar/edu/itba/mygymapp/ui/register/register.java
+++ app/src/main/java/ar/edu/itba/mygymapp/ui/register/register.java
@@ -1,16 +1,67 @@
 package ar.edu.itba.mygymapp.ui.register;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import ar.edu.itba.mygymapp.MainActivity;
 import ar.edu.itba.mygymapp.R;
+import ar.edu.itba.mygymapp.databinding.ActivityRegisterBinding;
 
 public class register extends AppCompatActivity {
+    private ActivityRegisterBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register);
+        binding=ActivityRegisterBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.registerBtn.setOnClickListener(this::registration);
+    }
+
+    private void registration(View view) {
+        EditText userView,mailView, passView, confirmPass;
+        String username, password, password2,mail;
+        boolean error=false;
+        userView=binding.username;
+        passView=binding.password;
+        mailView= binding.editTextTextEmailAddress;
+        confirmPass=binding.password2;
+        username=userView.getText().toString();
+        mail=mailView.getText().toString();
+        password=passView.getText().toString();
+        password2=confirmPass.getText().toString();
+
+        if(username.trim().length()==0){
+            userView.setError(getText(R.string.invalid_username));
+            error = true;
+        }
+        if(mail.trim().length()==0){
+            mailView.setError(getText(R.string.invalid_email));
+            error = true;
+        }
+        if (password.trim().length() == 0) {
+            passView.setError(getText(R.string.invalid_password));
+            error = true;
+        }
+        if (password.compareTo(password2)!=0) {
+            confirmPass.setError(getText(R.string.invalid_password_confirmation));
+            error = true;
+        }
+        if (error) {
+            Toast.makeText(getApplicationContext(), getText(R.string.invalid_password_confirmation), Toast.LENGTH_SHORT).show();
+            return;
+        }
+        goToMainActivity();
+    }
+
+    public void goToMainActivity() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }

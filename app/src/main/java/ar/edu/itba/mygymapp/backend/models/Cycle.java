35fb85d5a0ab1bbed780af861639014da9fa56a0
Santiago Sandrini <ssandrini@itba.edu.ar>
2021-11-05 09:50:16 -0300

    Cycle and Exercises backend

---
+++ app/src/main/java/ar/edu/itba/mygymapp/backend/models/Cycle.java
@@ -0,0 +1,79 @@
+package ar.edu.itba.mygymapp.backend.models;
+
+public class Cycle {
+
+    private int id;
+    private String name;
+    private String detail;
+    private String type;
+    private int order;
+    private int repetitions;
+    private Object metadata;
+
+    public Cycle(int id, String name, String detail, String type, int order, int repetitions, Object metadata) {
+        this.id = id;
+        this.name = name;
+        this.detail = detail;
+        this.type = type;
+        this.order = order;
+        this.repetitions = repetitions;
+        this.metadata = metadata;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDetail() {
+        return detail;
+    }
+
+    public void setDetail(String detail) {
+        this.detail = detail;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getOrder() {
+        return order;
+    }
+
+    public void setOrder(int order) {
+        this.order = order;
+    }
+
+    public int getRepetitions() {
+        return repetitions;
+    }
+
+    public void setRepetitions(int repetitions) {
+        this.repetitions = repetitions;
+    }
+
+    public Object getMetadata() {
+        return metadata;
+    }
+
+    public void setMetadata(Object metadata) {
+        this.metadata = metadata;
+    }
+
+}

caa552ebf677c5bf32b4fc90077e3561f0fea5c3
Leonardo <ldagostino@itba.edu.ar>
2021-11-09 19:27:11 -0300

    back funcionando!!!!! casi q no se como

--- app/src/main/java/ar/edu/itba/mygymapp/ui/routines/RoutineActivity.java
+++ app/src/main/java/ar/edu/itba/mygymapp/ui/routines/RoutineActivity.java
@@ -81,4 +81,11 @@ public class RoutineActivity extends AppCompatActivity {
         return super.onCreateOptionsMenu(menu);
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }

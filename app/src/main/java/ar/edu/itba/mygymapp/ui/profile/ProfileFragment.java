a2794aa8fe0410ca141edfaa903dfeba614c8e0a
Santiago Sandrini <ssandrini@itba.edu.ar>
2021-11-09 13:46:30 -0300

    Fix

--- app/src/main/java/ar/edu/itba/mygymapp/ui/profile/ProfileFragment.java
+++ app/src/main/java/ar/edu/itba/mygymapp/ui/profile/ProfileFragment.java
@@ -148,7 +148,6 @@ public class ProfileFragment extends Fragment implements AdapterView.OnItemSelec
     @Override
     public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
         String text = adapterView.getItemAtPosition(i).toString();
-        Toast.makeText(adapterView.getContext(),text,Toast.LENGTH_SHORT).show();
     }
 
     @Override

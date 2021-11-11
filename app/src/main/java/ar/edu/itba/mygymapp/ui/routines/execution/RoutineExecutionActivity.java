0e0c907d849a0f82f366a410843f5eae6475cb8b
Leonardo <ldagostino@itba.edu.ar>
2021-11-11 03:00:01 -0300

    timer re cheto

--- app/src/main/java/ar/edu/itba/mygymapp/ui/routines/execution/RoutineExecutionActivity.java
+++ app/src/main/java/ar/edu/itba/mygymapp/ui/routines/execution/RoutineExecutionActivity.java
@@ -9,6 +9,9 @@ import android.view.View;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.mut_jaeryo.circletimer.CircleTimer;
+
+import ar.edu.itba.mygymapp.R;
 import ar.edu.itba.mygymapp.backend.models.Cycle;
 import ar.edu.itba.mygymapp.backend.models.CycleExercise;
 import ar.edu.itba.mygymapp.backend.models.Routine;
@@ -23,6 +26,10 @@ public class RoutineExecutionActivity extends AppCompatActivity {
     private int cycleIndex = 0;
     private int exerciseIndex = 0;
     private int cyclesLength;
+    private CircleTimer timer;
+    private boolean runningTimer = true;
+    private boolean started = true;
+
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,49 +44,106 @@ public class RoutineExecutionActivity extends AppCompatActivity {
 
         setExercise(routine.getCycles().get(0).getExercises().get(0));
         setCycle(routine.getCycles().get(0));
+        timer = findViewById(R.id.timer);
+
+        timer.setBaseTimerEndedListener(new CircleTimer.baseTimerEndedListener() {
+            @Override
+            public void OnEnded() {
+                new Next().onClick(null);
+            }
+        });
+
+        startTimer(routine.getCycles().get(0).getExercises().get(0));
+
+        binding.timerLayout.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+
+                if (runningTimer) {
+                    pauseTimer();
+                } else {
+                    resumeTimer();
+                }
+                runningTimer = !runningTimer;
+
+            }
+
+
+        });
+
+
 //        Intent i = getIntent();
 //        routine = (Routine) i.getSerializableExtra("abc");
 
 
-        binding.previousBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+        binding.previousBtn.setOnClickListener(new Back());
 
-                if (exerciseIndex == 0) {
-                    if (cycleIndex == 0) {
-                        return ;
-                    }
-                    cycleIndex--;
-                    setCycle(routine.getCycles().get(cycleIndex));
-                    exerciseIndex = routine.getCycles().get(cycleIndex).getExercises().size() - 1;
-                } else {
-                    exerciseIndex--;
+        binding.nextBtn.setOnClickListener(new Next());
+    }
 
+    private class Next implements View.OnClickListener {
+
+        @Override
+        public void onClick(View view) {
+            int cycleExercisesLength = routine.getCycles().get(cycleIndex).getExercises().size();
+            int cyclesLimit = cyclesLength - 1;
+
+            if (exerciseIndex == cycleExercisesLength - 1) {
+                if (cycleIndex == cyclesLimit) {
+                    return;
                 }
-                setExercise(routine.getCycles().get(cycleIndex).getExercises().get(exerciseIndex));
+                cycleIndex++;
+                setCycle(routine.getCycles().get(cycleIndex));
+                exerciseIndex = 0;
+            } else {
+                exerciseIndex++;
+            }
+            setExercise(routine.getCycles().get(cycleIndex).getExercises().get(exerciseIndex));
+            startTimer(routine.getCycles().get(cycleIndex).getExercises().get(exerciseIndex));
+        }
+    }
+
+    private class Back implements View.OnClickListener {
+
+        @Override
+        public void onClick(View view) {
+
+            if (exerciseIndex == 0) {
+                if (cycleIndex == 0) {
+                    return;
+                }
+                cycleIndex--;
+                setCycle(routine.getCycles().get(cycleIndex));
+                exerciseIndex = routine.getCycles().get(cycleIndex).getExercises().size() - 1;
+            } else {
+                exerciseIndex--;
 
             }
-        });
+            setExercise(routine.getCycles().get(cycleIndex).getExercises().get(exerciseIndex));
+            startTimer(routine.getCycles().get(cycleIndex).getExercises().get(exerciseIndex));
+        }
+    }
 
-        binding.nextBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                int cycleExercisesLength = routine.getCycles().get(cycleIndex).getExercises().size();
-                int cyclesLimit = cyclesLength - 1;
 
-                if (exerciseIndex == cycleExercisesLength - 1) {
-                    if (cycleIndex == cyclesLimit) {
-                        return ;
-                    }
-                    cycleIndex++;
-                    setCycle(routine.getCycles().get(cycleIndex));
-                    exerciseIndex = 0;
-                } else {
-                    exerciseIndex++;
-                }
-                setExercise(routine.getCycles().get(cycleIndex).getExercises().get(exerciseIndex));
-            }
-        });
+    private void resumeTimer() {
+        binding.playBtn.setVisibility(View.GONE);
+        binding.pauseBtn.setVisibility(View.VISIBLE);
+        timer.start();
+    }
+
+    private void pauseTimer() {
+        binding.pauseBtn.setVisibility(View.GONE);
+        binding.playBtn.setVisibility(View.VISIBLE);
+        timer.stop();
+    }
+
+    private void startTimer(CycleExercise cycleExercise) {
+        timer.setMaximumTime(cycleExercise.getDuration());
+        timer.setInitPosition(cycleExercise.getDuration());
+        timer.start();
+        binding.playBtn.setVisibility(View.GONE);
+        binding.pauseBtn.setVisibility(View.VISIBLE);
     }
 
     private void setExercise(CycleExercise exercise) {
@@ -94,6 +158,8 @@ public class RoutineExecutionActivity extends AppCompatActivity {
         repsSb.append('x');
         repsSb.append(String.valueOf(exercise.getRepetitions()));
         binding.exerciseReps.setText(repsSb.toString());
+
+//        startTimer(exercise);
     }
 
     private void setCycle(Cycle cycle) {
